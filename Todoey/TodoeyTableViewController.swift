838120e9c39abe3a6d24b078cc4f41dcad2802a1
Hadi <hadi@Hadis-Mac.local>
2018-08-04 18:48:23 +0500

    add new item button + functionality completed

--- Todoey/TodoeyTableViewController.swift
+++ Todoey/TodoeyTableViewController.swift
@@ -10,12 +10,14 @@ import UIKit
 
 class TodoeyTableViewController: UITableViewController {
 
-      let itemArray = ["Milk", "Eggs", "Bread"]
+      var itemArray = ["Milk", "Eggs", "Bread"]
     override func viewDidLoad() {
         super.viewDidLoad()
       
         // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    //MARK - table view delegate methods
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return itemArray.count
     }
@@ -33,9 +35,25 @@ class TodoeyTableViewController: UITableViewController {
         }
         else{
             tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
-
         }
-
     }
+    //MARK - add item Button
+    @IBAction func addButtonPressed(_ sender: UIBarButtonItem) {
+        var textField = UITextField()
+        let alert = UIAlertController(title: "Add Todoey Item", message: nil, preferredStyle: .alert)
+        let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+           // what will happen when the user will tap on the add button
+            self.itemArray.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        alert.addTextField { (alertTextField) in
+            alertTextField.placeholder = "Create New Item"
+            textField = alertTextField
+           
+        }
+        alert.addAction(action)
+        present(alert, animated: true, completion: nil)
+    }
+    
 }
 
